db2d1516fdfcf2e0c3e38b10792e6366c3710ff9
Dragan <iliya5376@mail.com>
2017-08-10 19:51:37 +0300

    feature(app.js) add response

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var Koa = require('koa');
 var app = new Koa();
 var logger = require('koa-logger');
+var serve = require('koa-static');
+var chalk = require('chalk');
 
 
 app.use(logger());
+app.use(serve(__dirname + '/static'));
+
 app.use(async (ctx) => {
 	ctx.body = 'Ce kavo';
 });
-app.listen(8000);
+
+
+var port = 8000;
+app.listen(port, ()=> {
+	console.log(chalk.yellow('Сервер работает на порту ' + port +'...'));
+});
